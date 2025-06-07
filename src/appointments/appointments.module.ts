63e0361a190b30debcf51104dfdd0d9b56ab60f6
Serhat Gürgenyatağı <63667747+Se-Gu@users.noreply.github.com>
2025-06-07 12:39:47 +0300

    Validate staff before appointment creation

--- src/appointments/appointments.module.ts
+++ src/appointments/appointments.module.ts
@@ -3,9 +3,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppointmentsService } from './appointments.service';
 import { AppointmentsController } from './appointments.controller';
 import { Appointment } from './entities/appointment.entity';
+import { Staff } from '../staff/entities';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Appointment])],
+  imports: [TypeOrmModule.forFeature([Appointment, Staff])],
   controllers: [AppointmentsController],
   providers: [AppointmentsService],
   exports: [AppointmentsService],
